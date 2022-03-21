b763c700225145f1ae908ac1b2d827069fa78a06
russelltli(李涛) <russeltli@tencent.com>
2022-03-21 12:57:21 +0800

    feat: add eslint-config package

---
+++ packages/all/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: [
+    '@litarussell/eslint-config-react',
+  ],
+}
--- packages/basic/index.js
+++ packages/basic/index.js
@@ -18,6 +18,15 @@ module.exports = {
 
   overrides: [
     {
+      files: ['*.json', '*.json5'],
+      parser: 'jsonc-eslint-parser',
+      rules: {
+        'quotes': ['error', 'double'],
+        'quote-props': ['error', 'always'],
+        'comma-dangle': ['error', 'never'],
+      },
+    },
+    {
       files: ['package.json'],
       parser: 'jsonc-eslint-parser',
       rules: {
